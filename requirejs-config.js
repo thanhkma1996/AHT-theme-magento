abb0b2d60a7b5f6b9c6532bcfa2ab422c8125355
Thanhkma1996 <soldiersoociu@gmail.com>
2019-01-15 17:15:07 +0700

    update theme magento

---
+++ requirejs-config.js
@@ -0,0 +1,17 @@
+var config = {
+    map: {
+          "*": {
+              "owlslider": "js/owlcarousel/owl.carousel.min"
+          }
+      },
+      paths:  {
+          "owlslider" : "js/owlcarousel/owl.carousel.min"    
+      },
+      "shim": {
+    "js/owlcarousel/owl.carousel.min": ["jquery"],
+   }
+   // deps: [
+   //        "Magento_Theme/js/legends",
+   //    ]
+    
+  };
